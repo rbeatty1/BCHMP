d979d25f35c058e16c5377435ea38af69c36627b
Michael Ruane <mruane@dvrpc.org>
2018-03-07 19:48:22 -0500

    DCE

--- webpack.config.js
+++ webpack.config.js
@@ -20,15 +20,6 @@ const config = {
             'esri-leaflet-renderers'
         ]
   },
-  // resolve: {
-  //       extensions: [ '.html', '.js', '.json', '.scss', '.css'],
-  //       alias: {
-  //           leaflet_css: __dirname + "/node_modules/leaflet/dist/leaflet.css",
-  //           leaflet_marker: __dirname + "/node_modules/leaflet/dist/images/marker-icon.png",
-  //           leaflet_marker_2x: __dirname + "/node_modules/leaflet/dist/images/marker-icon-2x.png",
-  //           leaflet_marker_shadow: __dirname + "/node_modules/leaflet/dist/images/marker-shadow.png"
-  //       }
-  //   },
   module: {
         rules: [    
             {
